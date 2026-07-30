23f8c53dd4c25a599d3627ef9cbf945fec8c47d3
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2026-07-31 00:32:55 +0200

    chore: add gamemanager
    Just to set the target framerate.

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+	public class GameManager : MonoBehaviour
+	{
+		private void Awake()
+		{
+			Application.targetFrameRate = (int)Screen.currentResolution.refreshRateRatio.value;
+			Debug.Log($"Target frame rate set to {Application.targetFrameRate} FPS");
+		}
+	}
+}
