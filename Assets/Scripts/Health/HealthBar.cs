c7b8d4cccae1569238bb9d05b7b7e08a2a1e958c
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2026-08-01 20:52:08 +0200

    feat: add first iteration of health bar

---
+++ Assets/Scripts/Health/HealthBar.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace Health
+{
+	public class HealthBar : MonoBehaviour
+	{
+		[Header("References")]
+		[SerializeField] private SpriteRenderer FillSprite;
+		[SerializeField] private SpriteRenderer BackgroundSprite;
+
+		private Transform _owner;
+		private float _fullWidth;
+		private float _fullLocalPositionX;
+
+		private void Awake()
+		{
+			if (!FillSprite)
+				return;
+
+			_fullWidth = FillSprite.transform.localScale.x;
+			_fullLocalPositionX = FillSprite.transform.localPosition.x;
+		}
+
+		public void SetHealth(float current, float max)
+		{
+			float percent = max > 0f ? current / max : 0f;
+			SetHealthPercent(percent);
+		}
+
+		public void SetHealthPercent(float percent)
+		{
+			percent = Mathf.Clamp01(percent);
+
+			if (!FillSprite)
+				return;
+
+			Transform fillTransform = FillSprite.transform;
+
+			Vector3 scale = fillTransform.localScale;
+			scale.x = _fullWidth * percent;
+			fillTransform.localScale = scale;
+
+			Vector3 localPosition = fillTransform.localPosition;
+			localPosition.x = _fullLocalPositionX - (_fullWidth - scale.x) * 0.5f;
+			fillTransform.localPosition = localPosition;
+		}
+	}
+}
